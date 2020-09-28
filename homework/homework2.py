f6339894625526ee2534aacae500a1963940c43e
moundu <munenemuondu@gmail.com>
2020-09-28 17:56:43 +0300

    I have made some changes

---
+++ homework/homework2.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+conn = sqlite3.connect('homework2.db')
+c = conn.cursor()
+
+c.execute('CREATE TABLE IF NOT EXISTS numbers(number INTEGER)')
+
+with conn:
+    number1 = int(input("Enter a number:  "))
+    c.execute('INSERT INTO numbers VALUES(?)',(number1,))
+
+
+list1 = [a for dub in c.execute('SELECT * FROM numbers') for a in dub]
+print(list1)
+cola = sum(list1)
+print(cola)
